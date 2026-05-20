24737cdfaaf4af62f8f4892776a3367c52dcecc8
weizheng <wei.zheng@pingcap.com>
2026-05-20 09:20:29 +0800

    test(prowjob): expect pull author label

--- pkg/pjutil/pjutil_test.go
+++ pkg/pjutil/pjutil_test.go
@@ -1064,6 +1064,7 @@ func TestNewPresubmitWithModifiers(t *testing.T) {
 						kube.RepoLabel:         "repo-name",
 						kube.BaseRefLabel:      "base-ref",
 						kube.PullLabel:         "42",
+						kube.AuthorLabel:       "user-login",
 						"extra-label":          "foo",
 					},
 					Annotations: map[string]string{
@@ -1130,6 +1131,7 @@ func TestNewPresubmitWithModifiers(t *testing.T) {
 						kube.RepoLabel:         "repo-name",
 						kube.BaseRefLabel:      "base-ref",
 						kube.PullLabel:         "42",
+						kube.AuthorLabel:       "user-login",
 						"extra-label":          "foo",
 					},
 					Annotations: map[string]string{
